258e09c3667072eebe93f4fa4122a817b9a06cf3
David Fisher <daveed201@gmail.com>
2021-04-27 18:59:25 -0500

    added basic character movement
    initialized unity project, added a few sprites, coded initial character
    movement

---
+++ Platformer/Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * movespeed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, movespeed);
+        }
+    }
+}
