bb0fc6e9b0ff8286870bbfbad5e41bd401aa6053
David Fisher <daveed201@gmail.com>
2021-06-01 20:56:36 -0500

    made it so player can only jump when in contact with another object

--- Platformer/Assets/Scripts/Character_Controller.cs
+++ Platformer/Assets/Scripts/Character_Controller.cs
@@ -10,18 +10,42 @@ public class Character_Controller : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     void Update()
     {
-
         float x = Input.GetAxisRaw("Horizontal");
         float moveBy = x * movespeed;
         rb.velocity = new Vector2(moveBy, rb.velocity.y);
-        if (Input.GetKey(KeyCode.UpArrow))
+        List < ContactPoint2D > contacts = new List <ContactPoint2D>();
+        if (rb.GetContacts(contacts) != 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, movespeed);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, movespeed);
+            }
         }
     }
 }
+
+
+
+//using UnityEngine;
+//
+//public class Example : MonoBehaviour
+//{
+//    CharacterController controller;
+//
+//    void Start()
+//    {
+//        controller = GetComponent<CharacterController>();
+//    }
+//
+//    void Update()
+//    {
+//        if (controller.isGrounded)
+//        {
+//            print("CharacterController is grounded");
+//        }
+//    }
+//}
